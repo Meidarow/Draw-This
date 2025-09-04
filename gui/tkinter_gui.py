ad0b88f170d23add9b3d074c0a12a601be563e19
Meidarow <60526952+Meidarow@users.noreply.github.com>
2025-09-03 23:15:01 -0300

    GUI tweaked

--- gui/tkinter_gui.py
+++ gui/tkinter_gui.py
@@ -24,9 +24,8 @@ def start_slideshow_gui():
         start_slideshow(selected_folders, geometry=None, drawing_time=selected_delay)
 
 def add_delay(delay,var):
-    timer = tk.Radiobutton(timer_frame, text=delay, variable=var, value=delay)
+    timer = tk.Radiobutton(timer_frame, text=f"{delay} seconds", variable=var, value=delay)
     timer.pack(side="left")
-
 
 class StdoutRedirector:
     def __init__(self, text_widget):
@@ -38,44 +37,56 @@ class StdoutRedirector:
         pass
 
 default_folders = ["/mnt/Storage/Art/Resources/Animals","/mnt/Storage/Art/Resources/Humans/Figures/Non-Nude"]
-default_delay = [35, 65, 5, 0]
+default_delay = [5, 32, 62, 302]
 
 root = tk.Tk()
 root.title("Draw-This Slideshow")
-root.geometry("1000x400")
+root.geometry("1000x600")
 
 folder_vars =[]
 delay_var = tk.IntVar(value=0)
 
-timer_frame = tk.Frame(root)
-timer_frame.pack(fill="both")
+# Frame central que segura tudo
+central_frame = tk.Frame(root)
+central_frame.place(relx=0.5, rely=0.5, anchor="center")
 
-folder_frame = tk.Frame(root)
-folder_frame.pack(fill="both", expand=True, padx=10,pady=10)
+# Timer section
+timer_frame = tk.Frame(central_frame)
+timer_frame.pack(fill="x", pady=10)
 
-console_frame = tk.Frame(root)
-console_frame.pack(fill="both", expand=True)
+timer_text = tk.Label(timer_frame, text="Select duration:")
+timer_text.pack()
 
-console_text = tk.Text(console_frame, height=10)
-console_text.pack(fill="both", expand=True)
+# Folder section
+folder_frame = tk.Frame(central_frame)
+folder_frame.pack(fill="both", expand=True, padx=10, pady=10)
 
-timer_text = tk.Label(timer_frame,text="Select duration (0 is infinite): ")
-timer_text.pack(side="left")
+folders_label = tk.Label(folder_frame, text="Select folders:")
+folders_label.pack()
+
+# Console section
+console_frame = tk.Frame(central_frame)
+console_frame.pack(fill="both", expand=True, pady=10)
+
+
+# Botões
+add_button = tk.Button(central_frame, text="Add folder", command=select_folder)
+add_button.pack(pady=5)
+
+start_button = tk.Button(central_frame, text="Start", command=start_slideshow_gui)
+start_button.pack(pady=5)
 
 for duration in default_delay:
     add_delay(str(duration),delay_var)
-timer_text_2 = tk.Label(timer_frame,text=" seconds")
-timer_text_2.pack(side="left")
+
+timer_inf = tk.Radiobutton(timer_frame, text="indefinite", variable=delay_var, value=0)
+timer_inf.pack(side="right")
 
 for folder in default_folders:
     add_folder(folder)
 
-add_button = tk.Button(root, text="Add folder", command=select_folder)
-add_button.pack(pady=5)
-
-start_button = tk.Button(root, text="Start", command=start_slideshow_gui)
-start_button.pack(pady=5)
-
+console_text = tk.Text(console_frame, height=10)
+console_text.pack(fill="both", expand=True)
 sys.stdout = StdoutRedirector(console_text)
 
 root.mainloop()
